feb69d7c14fe36a829f6bca15375acd0f7d16ade
Vladislav Kalman <Vladislav_99355@gmail.com>
2021-11-25 01:06:57 +0300

    resize buttons

--- checker.py
+++ checker.py
@@ -2,6 +2,7 @@ import socket
 import stun
 
 
+# TODO: add time checkout
 def get_host_ip():
     return stun.get_ip_info()  # get host ip (ip from net)
 
@@ -9,9 +10,9 @@ def get_host_ip():
 def port_check(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     check = (ip, int(port))
-    result = sock.connect_ex(check)  # 51413
+    result = sock.connect_ex(check)
     if result == 0:
-        return ('Порт ' + str(port) + ' открыт ' + ip)
-    else:
         return ('Порт ' + str(port) + ' закрыт ' + ip)
+    else:
+        return ('Порт ' + str(port) + ' открыт ' + ip)
     sock.close()
--- teleg_bot.py
+++ teleg_bot.py
@@ -4,20 +4,21 @@ import rotate
 bot = telebot.TeleBot('2113531172:AAHQYikxVcodytj5l3vdLdQ8Lkp5T9HUqRc')
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+#     bot.reply_to(message, "Howdy, how are you doing?")
 
-markup = types.ReplyKeyboardMarkup()
-itembtn = types.KeyboardButton('check ports')
-markup.row(itembtn)
-# tb.send_message(chat_id, "Choose one letter:", reply_markup=markup)
+# buttons for quickly send request
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+itembtn_1 = types.KeyboardButton('check ports')
+itembtn_2 = types.KeyboardButton('help')
+markup.row(itembtn_1, itembtn_2)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "check ports":
         for i in rotate.run_rotate():
-            bot.send_message(message.from_user.id, i)
+            bot.send_message(message.from_user.id, i, reply_markup=markup)
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши  /help.")
 
