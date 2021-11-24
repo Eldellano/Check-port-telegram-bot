e0ee3949534c4afa233d205cffe81c18eedb2411
Vladislav Kalman <Vladislav_99355@gmail.com>
2021-11-25 01:46:18 +0300

    add timeout

--- checker.py
+++ checker.py
@@ -9,8 +9,9 @@ def get_host_ip():
 
 def port_check(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    check = (ip, int(port))
-    result = sock.connect_ex(check)
+    sock.settimeout(2)  # 2 second timeout
+    result = sock.connect_ex((ip, int(port)))
+    print(result, ip)
     if result == 0:
         return ('Порт ' + str(port) + ' закрыт ' + ip)
     else:
