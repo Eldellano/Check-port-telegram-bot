2aea43a011ea5bfd48d2a4da3123602597009c91
Vladislav Kalman <Vladislav_99355@gmail.com>
2021-11-25 22:29:59 +0300

    fix code

--- checker.py
+++ checker.py
@@ -7,7 +7,7 @@ def get_host_ip():
     return stun.get_ip_info()  # get host ip (ip from net)
 
 
-def port_check(ip, port):
+def port_check(ip, port):  # via https://stackoverflow.com/a/19196218
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(2)  # second timeout
     result = sock.connect_ex((ip, int(port)))
