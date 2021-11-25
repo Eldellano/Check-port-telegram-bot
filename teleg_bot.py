8b9cd83f9f187917fa10f707f799ed8220722340
Vladislav Kalman <Vladislav_99355@gmail.com>
2021-11-25 21:38:07 +0300

    waiting reconnect if no internet

--- teleg_bot.py
+++ teleg_bot.py
@@ -1,8 +1,8 @@
 from telebot import *
 import rotate
+import time
 
 bot = telebot.TeleBot('2113531172:AAHQYikxVcodytj5l3vdLdQ8Lkp5T9HUqRc')
-
 
 # @bot.message_handler(commands=['start', 'help'])
 # def send_welcome(message):
@@ -14,6 +14,7 @@ itembtn_1 = types.KeyboardButton('check ports')
 itembtn_2 = types.KeyboardButton('help')
 markup.row(itembtn_1, itembtn_2)
 
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "check ports":
@@ -23,4 +24,10 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши  /help.")
 
 
-bot.polling(none_stop=True, interval=0)
+# run bot, waiting reconnect if no internet
+while True:
+    try:
+
+        bot.polling(none_stop=True, interval=0, timeout=30)
+    except:
+        time.sleep(5)
