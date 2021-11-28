fa97d583bf4bc7476490b59d89dc63c7d9628b93
Vladislav Kalman <Vladislav_99355@gmail.com>
2021-11-28 22:28:19 +0300

    add database

--- port_data.py
+++ port_data.py
@@ -10,3 +10,13 @@ class DataBase:
     def port_get(self):
         self.cursor.execute('select port, name from net_ports')
         return self.cursor.fetchmany(10)
+
+    def port_add(self, port, name):
+        self.cursor.execute('insert into net_ports (port, name) values (?, ?)', [port, name])
+        self.conn.commit()
+
+    def port_del(self, port):
+        self.cursor.execute('delete from net_ports where port = (?)', [port])
+        self.conn.commit()
+
+DataBase().port_del('9091')
