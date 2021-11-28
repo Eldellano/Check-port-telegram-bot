47895267c1a97d70900dd517e0e024d5bbe99237
Vladislav Kalman <Vladislav_99355@gmail.com>
2021-11-28 22:28:39 +0300

    add new buttons for user

--- teleg_bot.py
+++ teleg_bot.py
@@ -1,34 +1,56 @@
-import telebot
 from telebot import *
 import rotate
 import time
+import re
+import port_data
 
 bot = telebot.TeleBot('2104899805:AAEfrx02aBs_9W7tRcovz-ZgleGO1hheeXQ')  # TEST BOT!!!
 
-# @bot.message_handler(commands=['start', 'help'])
-# def send_welcome(message):
-#     bot.reply_to(message, "Howdy, how are you doing?")
 
 # buttons for quickly send request
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 itembtn_1 = types.KeyboardButton('check ports !test!')
-itembtn_2 = types.KeyboardButton('help')
+itembtn_2 = types.KeyboardButton('Дополнительно')
 markup.row(itembtn_1, itembtn_2)
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Бот позволяет отслеживать статус твоих сетевых портов. Порты для отслеживания можно добавить и удалить.")
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "check ports !test!":
+    if message.text == 'check ports !test!':
         for i in rotate.run_rotate():
             bot.send_message(message.from_user.id, i, reply_markup=markup)
+    elif message.text == 'Дополнительно':
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = ['Добавить порт', 'Удалить порт', 'Назад']
+        keyboard.row(*buttons)
+        bot.send_message(message.from_user.id, 'выбери пункт', reply_markup=keyboard)
+    elif message.text == 'Добавить порт':
+        keyboard = types.ReplyKeyboardRemove(selective=False)
+        bot.send_message(message.from_user.id, 'Введи порт и емя сервиса через пробел', reply_markup=keyboard)
+    elif message.text == 'Удалить порт':
+        keyboard = types.ReplyKeyboardRemove(selective=False)
+        bot.send_message(message.from_user.id, 'Введи порт', reply_markup=keyboard)
+    # add new port
+    elif re.search(r'([0-9]+)(.){1}(\D+)', message.text):
+        match = re.search(r'([0-9]+)(.){1}(\D+)', message.text)
+        port_data.DataBase().port_add(match.group(1), match.group(3))
+        bot.send_message(message.from_user.id, 'Порт добавлен', reply_markup=markup)
+    # delete port
+    elif re.search(r'([0-9]+)', message.text):
+        match = re.search(r'([0-9]+)', message.text)
+        port_data.DataBase().port_del(match.group(1))
+        bot.send_message(message.from_user.id, 'Порт удален', reply_markup=markup)
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши  /help.")
+        bot.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши  /help.')
 
 
-# run bot, waiting reconnect if no internet
-# while True:
-#     try:
-#         bot.polling(none_stop=True, interval=0, timeout=30)
-#     except:  # TODO: add catch ConnectionError
-#         time.sleep(6)
-bot.polling(none_stop=True, interval=0, timeout=30)
+#run bot, waiting reconnect if no internet
+while True:
+    try:
+        bot.polling(none_stop=True, interval=0, timeout=30)
+    except:  # TODO: add catch ConnectionError
+        time.sleep(6)
+# bot.polling(none_stop=True, interval=0, timeout=30)
